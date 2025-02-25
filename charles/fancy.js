184fd2d1754ebd50c2f4ae8a1c304c76f1f5bf0f
Ibrahim Adams <162009674+ibrahimaitech@users.noreply.github.com>
2025-02-25 18:59:46 +0300

    .

--- charles/fancy.js
+++ charles/fancy.js
@@ -8,7 +8,7 @@ zokou({ nomCom: "fancy", categorie: "Fun", reaction: "〽️" }, async (dest, zk
 
     try {
         if (id === undefined || text === undefined) {
-            return await repondre(`\nExemple : ${prefixe}fancy 10 Charles-Md\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('CHARLES-MD', fancy));
+            return await repondre(`\nExemple : ${prefixe}fancy 10 TOXIC-LOVER-MD\n` + String.fromCharCode(8206).repeat(4001) + fancy.list('TOXIC-LOVER-MD', fancy));
         }
 
         const selectedStyle = fancy[parseInt(id) - 1];
